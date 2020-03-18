f994db27af6fe244a5ca0919a0b77f5c56f93bec
Leina Pulgarin <leinatamayo@gmail.com>
2020-03-18 01:33:39 -0500

    Sprint Dos trabajo virtual(casi terminado )

--- eventos/script/retoCinco.js
+++ eventos/script/retoCinco.js
@@ -1,2 +1,27 @@
 //tomo el elemento que deso modificar del html
 const targeta = document.querySelector(".target");
+const textModificar = document.querySelector("text-ocultar");
+
+// targeta.classList.add("teacher--selected");
+// textModificar.classList.add("favorite");
+
+const change = () => {
+    if (
+        targeta.querySelector(".target").classList.contains("teacher--selected")
+    ) {
+        targeta.classList.remove("teacher--selected");
+    } else {
+        targeta.classList.add("teacher--selected");
+    }
+};
+targeta.addEventListener("click", change);
+
+const texto = () => {
+    if (textModificar.textContent == "Añadir") {
+        textModificar.textContent == "Quitar";
+    } else {
+        textModificar.textContent == "Añadir";
+    }
+};
+textModificar.addEventListener("click", texto);
+// document.querySelector(".target").classList.contains("teacher--selected");
