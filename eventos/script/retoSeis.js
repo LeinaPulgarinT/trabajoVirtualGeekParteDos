92e3f40a648fca32d63c02cf8b76b1a4a01eb492
Leina Pulgarin <leinatamayo@gmail.com>
2020-03-18 04:15:28 -0500

    Sprint Dos(ultima modificacion es reto6 eventos

--- eventos/script/retoSeis.js
+++ eventos/script/retoSeis.js
@@ -3,7 +3,7 @@ const cambiarBg = document.querySelector(".body");
 const change = event => {
     if (event.keyCode === 82) {
         cambiarBg.classList.add("green");
-        cambiarBg.remove("pink");
+        cambiarBg.classList.remove("pink");
     }
     if (event.keyCode === 77) {
         cambiarBg.classList.add("pink");
